a3cce244d39b8025fdfceda98fc899a1d933aed7
afayelaz <afayelaz@gmail.com>
2023-08-25 23:23:17 -0500

    Added LogicalOps.java // && -and, || - or , ! -not

---
+++ JavaProgramming/src/practise/LogicalOps.java
@@ -0,0 +1,11 @@
+package practise;
+
+public class LogicalOps {
+    public static void main(String[] args) {
+
+        // && -and, || - or , ! -not
+
+        boolean res1 = 10>5 && 9>8;
+        System.out.println(res1);
+    }
+}
