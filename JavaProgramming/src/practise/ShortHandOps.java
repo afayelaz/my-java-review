3cc73d28defadd71d1835d02c725ac53fc5754ea
afayelaz <afayelaz@gmail.com>
2023-08-25 23:20:54 -0500

    Added ShortHandOps.java  x +=y  // x+y

---
+++ JavaProgramming/src/practise/ShortHandOps.java
@@ -0,0 +1,21 @@
+package practise;
+
+public class ShortHandOps {
+
+    public static void main(String[] args) {
+        /*
+            x +=y  // x+y
+            x -=y // x-y
+            x /=y // x/y
+            x *=y // x*y
+            x %=y // x%y
+         */
+
+        int e = 20 ;
+        System.out.println(e);
+        e /= 4;
+        System.out.println("20 divided 4 = " + e);
+
+
+    }
+}
