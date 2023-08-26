32eea62138ac89a21a81681ade5477670ee5b7e9
afayelaz <afayelaz@gmail.com>
2023-08-25 23:12:30 -0500

    added EscapeSequences.java \n new line \t horizontal tab \ backslash only double quo -"Java"

---
+++ JavaProgramming/src/practise/EscapeSequences.java
@@ -0,0 +1,13 @@
+package practise;
+
+public class EscapeSequences {
+
+    public static void main(String[] args) {
+        System.out.println("Hi,How are u \nGood"); // \n new line next line printing
+        System.out.println("\tJava is fun"); // \ horizontal tab nex tab
+        System.out.println("\\"); // backslash only to be printed \\
+        System.out.println("My fav prog lang is \"Java\""); // \" double quotation mark printed "Java"
+
+    }
+
+}
