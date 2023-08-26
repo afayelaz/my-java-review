4f315defd9868001d1eb13d8ad0f730e913d36a2
afayelaz <afayelaz@gmail.com>
2023-08-25 22:58:30 -0500

    added JavaProgramming module - HelloWorld.java

---
+++ JavaProgramming/src/practise/HelloWorld.java
@@ -0,0 +1,8 @@
+package practise;
+
+public class HelloWorld {
+    public static void main (String args[]) {
+        System.out.println("Hello World!");
+        System.out.println("Hi"); //sout !!
+    }
+}
