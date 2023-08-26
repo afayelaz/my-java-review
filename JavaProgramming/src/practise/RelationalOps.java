c4b7756682c600f7aac06a8ab991a04471fa0247
afayelaz <afayelaz@gmail.com>
2023-08-25 23:22:03 -0500

    Added RelationalOps.java // > , >= , < , <= , ==, !=

---
+++ JavaProgramming/src/practise/RelationalOps.java
@@ -0,0 +1,18 @@
+package practise;
+
+public class RelationalOps {
+
+    public static void main(String[] args) {
+        // > , >= , < , <= , ==, !=
+
+        boolean res1 = 10>11;
+        System.out.println(res1);
+
+        boolean res2 = 10==11;
+        System.out.println(res2);
+
+        boolean res3 = 10!=11;
+        System.out.println(res3);
+
+    }
+}
