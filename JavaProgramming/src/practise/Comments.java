89bdc66eafef8243bb6d7fa60d9be1ff3ebb9676
afayelaz <afayelaz@gmail.com>
2023-08-25 23:04:41 -0500

    added Comments.java - single line and multi line

---
+++ JavaProgramming/src/practise/Comments.java
@@ -0,0 +1,17 @@
+package practise;
+
+public class Comments {
+
+    public static void main (String args []) {
+
+        //single line comments
+        System.out.println("Hi All");
+
+/*
+        1.Hey
+                2.TEst
+                3.retest
+ */
+
+    }
+}
